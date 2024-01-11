2c3f68b4eda45e43ee87b76af9e5940e41896b6d
weijie <qoo77823@hotmail.com>
2024-01-11 16:42:03 +0800

    feat: from end URL validate

---
+++ public/javascripts/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const urlForm = document.querySelector('#url-form')
+
+// url valid function
+function isValidHttpUrl(urlString) {
+  try {
+    const newUrl = new URL(urlString);
+    return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
+  } catch (err) {
+    return false;
+  }
+}
+
+// from end URL validate
+urlForm.addEventListener('submit', (submitEvent) => {
+  submitEvent.preventDefault()
+  const urlString = document.querySelector('#url-string').value
+  if (isValidHttpUrl(urlString)) {
+    urlForm.submit()
+  } else {
+    alert('The URL you entered does not comply with the rules.')
+  }
+})
